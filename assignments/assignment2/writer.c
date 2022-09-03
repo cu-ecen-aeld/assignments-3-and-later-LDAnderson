72fe88c069bbdf89c02a89a76d92a65e9bde6fad
L. D. Anderson <landerson2@gmx.com>
2022-09-03 07:24:31 -0400

    Initial Commit

---
+++ assignments/assignment2/writer.c
@@ -0,0 +1,37 @@
+/* Writes a string to a file
+   usage: writer FULL_PATH STRING
+   Leif Anderson 
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/syslog.h>
+#include <syslog.h>
+
+void writeFile(char *filename, char *s) {
+  FILE* fp = NULL;
+
+  syslog(LOG_DEBUG, "Writing %s to %s", filename, s);
+
+  fp = fopen(filename, "w");
+  if(fp == NULL) {
+    syslog(LOG_ERR, "Cannot open file");
+  }
+
+  fprintf(fp, s);
+  fclose(fp);
+}
+
+int main(int argc, char *argv[]) {
+  openlog(NULL, 0, LOG_USER);
+
+  if(argc != 3) {
+    syslog(LOG_ERR, "Invalid arguments");
+    exit(-1);
+  }
+
+  char *filename = argv[1];
+  char *s = argv[2];
+  writeFile(filename, s);
+
+  return 0;
+}
